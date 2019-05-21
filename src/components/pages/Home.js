6c1603faff30dae960cfc39c031761dcc95ad2e8
Brian Bolnick <brianbolnick@gmail.com>
2019-05-21 14:03:23 -0600

    initial commit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import Layout from "components/common/Layout/Layout";
+import styled from "styled-components";
+//import Button from "components/common/Button/Button";
+import { Link } from "react-router-dom";
+import { rufina, media } from "styles/css-variables";
+//import FlashMessage from "components/common/FlashMessage/FlashMessage";
+
+//const TabLink = styled.div`
+  //font-size: 1.2rem;
+//`;
+
+//const TabsContainer = styled.div`
+  //display: flex;
+  //margin-right: 16px;
+  //${media.phone`
+	//margin-right: 0;
+	//margin: 16px auto;
+//`}
+//`;
+
+//const NewButtonContainer = styled.div``;
+//const OptionsContainer = styled.div`
+  //display: flex;
+  //align-items: center;
+  //${media.phone`
+    //flex-flow: column;
+//`}
+//`;
+
+//const Heading = styled.div`
+  //font-family: ${rufina};
+  //font-size: 1.8rem;
+  //white-space: nowrap;
+  //overflow: hidden;
+  //text-overflow: ellipsis;
+//`;
+
+//const Header = styled.div`
+  //display: flex;
+  //justify-content: space-between;
+  //align-items: center;
+
+  //margin-bottom: 32px;
+  //${media.phone`
+    //flex-flow: column;
+//`}
+//`;
+
+const Home = () => {
+  return (
+    <Layout>
+		Hello
+            </Layout>
+  );
+};
+
+export default Home;
--- src/components/pages/HomePage.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Layout from "components/common/Layout/Layout";
-
-export default () => {
-  return <Layout>Something</Layout>;
-};
